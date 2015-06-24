ec7974093c0e69cee012ccc78288f8164b3b4b58
tdoolan <tim.doolan@gmail.com>
2015-06-24 11:23:18 +0200

    Fixed settings when user is not logged in

--- apps/helloworld/views.py
+++ apps/helloworld/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
+    prefix = request.GET.get("Prefix")
+    settings = {}
+    if prefix != None:
+        settings["prefix"] = prefix
     return render(request, "helloworld/index.html",
             {"name": request.user.username,
-            "prefix": request.GET.get("Prefix")})
+            "settings": settings})
