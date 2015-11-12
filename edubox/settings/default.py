e0e27aa4faeff7fd46458d55e12e3fed0422854d
mslatour <sanderlatour@gmail.com>
2015-11-12 11:50:20 +0100

    Split settings in default and development

--- edubox/settings/default.py
+++ edubox/settings/default.py
@@ -96,18 +96,6 @@ STATICFILES_FINDERS = (
 
 WSGI_APPLICATION = 'edubox.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
@@ -120,12 +108,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-
-SITE_ID = 1
