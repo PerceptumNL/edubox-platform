01758823146f77d7c2d9d2fd354a0a1e446b3fe8
mslatour <sanderlatour@gmail.com>
2015-11-12 11:53:27 +0100

    Fixed production dependency on old existance of DATABASE setting in default.

--- edubox/settings/production.py
+++ edubox/settings/production.py
@@ -1,10 +1,10 @@
 from .default import *
 
-DEBUG = bool(int(os.environ.get('DEBUG_FLAG','0')))
+DEBUG = bool(int(os.environ.get('DEBUG_FLAG', '0')))
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {"default": dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
