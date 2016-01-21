0c8c002e1b06d940c2db6504543bec9e7842006c
mslatour <sanderlatour@gmail.com>
2016-01-21 17:33:06 +0100

    Added server cookie import in remote request

--- router/__init__.py
+++ router/__init__.py
@@ -266,6 +266,11 @@ class BaseRouter():
         return path
 
     def get_remote_request_cookiejar(self):
+        server_cookies = ServerCookie.objects.all(
+                domain=self.remote_domain,
+                user=self.request.user)
+        for server_cookie in server_cookies:
+            self.request.COOKIES[server_cookie.name] = server_cookie.value
         cookiejar = requests.utils.cookiejar_from_dict(self.request.COOKIES)
         self.debug("Remote request cookiejar: %s" % (cookiejar,))
         return cookiejar
