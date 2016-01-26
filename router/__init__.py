f96ab92f9a13443510f020f1877c8b344854f2da
mslatour <sanderlatour@gmail.com>
2016-01-26 01:17:58 +0100

    Added sent headers to login request debug.

--- router/__init__.py
+++ router/__init__.py
@@ -644,9 +644,10 @@ class AppRouter(Router):
             data=login_payload,
             headers=login_headers,
             url=login_url)
-        self.debug("[App login] Request:\n%s %s\n\n%s" % (
+        self.debug("[App login] Request:\n%s %s\n%s\n\n%s" % (
             response.request.method,
             response.request.url,
+            response.request.headers,
             response.request.body.replace(credentials.password, "****")))
         self.debug("[App login] Response code from login: %d"  % (
             response.status_code,))
