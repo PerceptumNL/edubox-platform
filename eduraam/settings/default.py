bbcb3afab5f4ab37f0537c55ad9b5f4c7e8e826e
mslatour <sanderlatour@gmail.com>
2016-02-17 17:30:43 +0100

    Set default authentication class to basis to allow for 401 response.

--- eduraam/settings/default.py
+++ eduraam/settings/default.py
@@ -121,6 +121,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ]
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication'
+    ]
 }
 
 # Internationalization
