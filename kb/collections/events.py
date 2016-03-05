5eeb8ccfd61387da0c380ba2e972cf43ee97a599
mslatour <sanderlatour@gmail.com>
2016-03-05 01:10:36 +0100

    Added event listener for completed activities.

---
+++ kb/collections/events.py
@@ -0,0 +1,20 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from django.http import QueryDict
+from kb.events.models import SubmittedEvent
+from .models import Activity, ActivityCompletion
+
+@receiver(post_save, sender=SubmittedEvent)
+def handle_activity_completion(sender, instance, **kwargs):
+    try:
+        activity = Activity.objects.get(url=instance.obj, app=instance.app);
+    except Activity.DoesNotExist:
+        return
+
+    try:
+        submission = QueryDict(instance.submission)
+        if submission.get('testResult',0) == 100:
+            ActivityCompletion.objects.get_or_create(
+                activity=activity, user=instance.user)
+    except Exception:
+        return;
