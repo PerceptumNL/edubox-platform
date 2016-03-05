221b0ff8d30062f803d4d2e440b875ad5f1466f6
mslatour <sanderlatour@gmail.com>
2016-03-05 01:45:28 +0100

    Fixed type conversion on activity completion.

--- kb/collections/events.py
+++ kb/collections/events.py
@@ -6,21 +6,15 @@ from .models import Activity, ActivityCompletion
 
 @receiver(post_save, sender=SubmittedEvent)
 def handle_activity_completion(sender, instance, **kwargs):
-    print('handle_activity_completion')
     try:
         activity = Activity.objects.get(url=instance.obj, app=instance.app);
     except Activity.DoesNotExist:
-        print('no activity')
         return
-
-    print('activity found')
 
     try:
         submission = QueryDict(instance.submission)
-        print(submission)
-        if submission.get('testResult',0) == 100:
+        if int(submission.get('testResult',0)) == 100:
             ActivityCompletion.objects.get_or_create(
                 activity=activity, user=instance.user)
     except Exception:
-        print('submission cannot be parsed')
         return;
