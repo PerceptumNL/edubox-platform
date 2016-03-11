367b266858163a5fb3e3a717d7fcfb1d7a3ed677
mslatour <sanderlatour@gmail.com>
2016-03-11 16:52:27 +0100

    Added custom admin for groups.

--- kb/groups/admin.py
+++ kb/groups/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent', 'institute')
+    list_filter = ('institute',)
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Institute)
 admin.site.register(Role)
 admin.site.register(Membership)
