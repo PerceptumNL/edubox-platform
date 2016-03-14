19dc7f42facc8133976e20c4b5656c4aac7648c0
mslatour <sanderlatour@gmail.com>
2016-03-14 11:10:54 +0100

    Added more useful userprofile admin

--- kb/admin.py
+++ kb/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import UserProfile
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'teacher', 'institute')
+    list_filter = ('institute',)
+    search_fields = (
+        'user__username',
+        'user__email',
+        'alias',
+        'user__last_name',
+        'user__first_name',
+        'institute__title')
+
+    def teacher(self, instance):
+        return instance.is_teacher()
+    teacher.boolean = True
+
+admin.site.register(UserProfile, UserProfileAdmin)
