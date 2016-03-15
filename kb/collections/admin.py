176135e767d847135d6a5a1ef1a19c5fb5c3e20a
mslatour <sanderlatour@gmail.com>
2016-03-15 10:59:57 +0100

    Added custom admin for activity completions.

--- kb/collections/admin.py
+++ kb/collections/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import LearningUnit, Activity, ActivityCompletion
 
+class ActivityCompletionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_full_name',
+                    'activity', 'datetime', 'institute')
+    list_filter = ('user__institute',)
+    search_fields = (
+        'user__user__username',
+        'user__user__email',
+        'user__alias',
+        'user__user__last_name',
+        'user__user__first_name',
+        'user__institute__title')
+
+    def user_full_name(self, instance):
+        return instance.user.full_name
+
+    def institute(self, instance):
+        return instance.user.institute.title
+
 admin.site.register(LearningUnit)
 admin.site.register(Activity)
-admin.site.register(ActivityCompletion)
+admin.site.register(ActivityCompletion, ActivityCompletionAdmin)
