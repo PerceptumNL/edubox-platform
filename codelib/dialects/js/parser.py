e152b065eccd6144065ba15c6c1b74538c4a49d1
mslatour <sanderlatour@gmail.com>
2016-04-05 14:59:35 +0200

    Added JS dialect parser.

--- codelib/dialects/js/parser.py
+++ codelib/dialects/js/parser.py
@@ -1,62 +1,109 @@
 import sys
 from antlr4 import *
-from ECMAScriptLexer import ECMAScriptLexer
-from ECMAScriptParser import ECMAScriptParser
-from ECMAScriptListener import ECMAScriptListener
+from .ECMAScriptLexer import ECMAScriptLexer
+from .ECMAScriptParser import ECMAScriptParser
+from .ECMAScriptListener import ECMAScriptListener
 
-class Parser(object):
+class ScriptListener(ECMAScriptListener):
 
-    class FuncPrinter(ECMAScriptListener):
-        def on_function(self, ctx):
-            print('###### Function ######')
-            print('Identifier:', ctx.Identifier())
-            parameter_list = ctx.formalParameterList()
-            if parameter_list is not None:
-                print('Arguments:', parameter_list.getText())
-            else:
-                print('Arguments: None')
-            body = ctx.functionBody()
-            print('# statements in body',
-                    body.sourceElements().getChildCount())
-            print('Body text:', repr(body.getText()))
-            print('\n')
+    dialect = None
 
-        def exitFunctionExpression(self, ctx):
-            self.on_function(ctx)
+    def __init__(self, dialect):
+        self.dialect = dialect
 
-        def exitFunctionDeclaration(self, ctx):
-            self.on_function(ctx)
+    def enterStatement(self, ctx):
+        self.dialect.lines += 1
 
-        def on_for(self, ctx):
-            print('###### For loop ######')
-            sequence = ctx.expressionSequence()
-            conditional = sequence[0].singleExpression(0)
-            increment = sequence[1].singleExpression(0)
-            print('Conditional:', conditional.getText())
-            print('Increment:', increment.getText())
-            print('Body:', ctx.statement().getText())
-            print('\n')
+    def exitFunctionExpression(self, ctx):
+        identifier = ctx.Identifier()
+        if identifier is not None:
+            identifier = identifier.getText()
 
-        def exitForStatement(self, ctx):
-            self.on_for(ctx)
+        arguments = ctx.formalParameterList()
+        if arguments is not None:
+            arguments = arguments.getText()
 
-        def exitForVarStatement(self, ctx):
-            self.on_for(ctx)
+        self.dialect.on_function(identifier, arguments)
 
-        def exitForInStatement(self, ctx):
-            self.on_for(ctx)
+    def exitFunctionDeclaration(self, ctx):
+        self.exitFunctionExpression(ctx)
 
-        def exitForVarInStatement(self, ctx):
-            self.on_for(ctx)
+    def exitForStatement(self, ctx):
+        sequence = ctx.expressionSequence()
+        conditional = sequence[0].singleExpression(0)
+        if conditional is not None:
+            conditional = conditional.getText()
 
-    def parse(self, filename):
-        print('Parsing', filename)
-        lexer = ECMAScriptLexer(FileStream(filename))
+        increment = sequence[1].singleExpression(0)
+        if increment is not None:
+            increment = increment.getText()
+
+        self.dialect.on_for(conditional, increment)
+
+    def exitForVarStatement(self, ctx):
+        self.exitForStatement(ctx)
+
+    def exitForInStatement(self, ctx):
+        self.exitForStatement(ctx)
+
+    def exitForVarInStatement(self, ctx):
+        self.exitForStatement(ctx)
+
+    def exitWhileStatement(self, ctx):
+        self.dialect.on_while(
+            ctx.expressionSequence().getChild(0).getText())
+
+    def exitIfStatement(self, ctx):
+        self.dialect.on_if(
+            ctx.expressionSequence().getChild(0).getText(),
+            bool(ctx.Else())
+        )
+
+class Dialect(object):
+    statements = None
+    """
+    A dictionary of statement types, each holding a list of tuples describing
+    each instance.
+
+    Types:
+     function - Function declaration: (identifier, arguments string)
+     for - For loop (conditional string, increment string)
+     while - While loop (conditional string,)
+     if - If (conditional string, has_else)
+    """
+    lines = 0
+
+    def __init__(self, filename, *args, **kwargs):
+        self.statements = { 'function': [],
+                            'for': [],
+                            'while': [],
+                            'if': []}
+        self.parse(filename)
+
+    def get(self, statement_type, filter_fn = None):
+        if filter_fn is None:
+            return self.statements[statement_type]
+        else:
+            return list(filter(filter_fn, self.statements[statement_type]))
+
+    def has(self, statement_type, filter_fn = None):
+        return bool(self.get(statement_type, filter_fn))
+
+    def on_function(self, identifier, arguments):
+        self.statements['function'].append((identifier, arguments))
+
+    def on_for(self, conditional, increment):
+        self.statements['for'].append((conditional, increment))
+
+    def on_while(self, conditional):
+        self.statements['while'].append((conditional,))
+
+    def on_if(self, conditional, has_else):
+        self.statements['if'].append((conditional, has_else))
+
+    def parse(self, code):
+        lexer = ECMAScriptLexer(InputStream(code))
         parser = ECMAScriptParser(CommonTokenStream(lexer))
-        printer = self.FuncPrinter()
+        listener = ScriptListener(self)
         walker = ParseTreeWalker()
-        walker.walk(printer, parser.program())
-
-if __name__ == '__main__':
-    parser = Parser()
-    parser.parse(sys.argv[1])
+        walker.walk(listener, parser.program())
