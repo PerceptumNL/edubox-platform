f4d993546ee71278bcfaecc51e3f51382e90a61c
mslatour <sanderlatour@gmail.com>
2016-04-18 10:05:11 +0200

    Fixed ordering of inbox messages to recent first.

--- kb/inbox/views.py
+++ kb/inbox/views.py
@@ -10,7 +10,7 @@ def message_list(request):
     from django.utils import timezone
     local_tz = timezone.get_current_timezone()
     from .models import Message
-    unread_messages = Message.objects.order_by('pk').filter(
+    unread_messages = Message.objects.order_by('-pk').filter(
         user=request.user.profile, read=False)
     for message in unread_messages:
         message_list.append({
